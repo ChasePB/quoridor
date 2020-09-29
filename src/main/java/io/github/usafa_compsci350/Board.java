b89e656062ae2a51aa62d58cc52cabbfe7eea044
Cristian Sullivan <cp.sully7@gmail.com>
2020-09-29 10:27:34 -0600

    Adding basic functionality

--- src/main/java/io/github/usafa_compsci350/Board.java
+++ src/main/java/io/github/usafa_compsci350/Board.java
@@ -1,19 +1,41 @@
 package io.github.usafa_compsci350;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class Board {
-  //  Store the walls for the board
-  //  ArrayList<Wall> walls = new ArrayList<Wall>();
-  //
-  //  Store the board's boxes
-  //  ArrayList<Square> squares = new ArrayList<Square>();
+  // Store the walls for the board
+  ArrayList<Wall> walls = new ArrayList<Wall>();
+
+  // Store the board's boxes
+  ArrayList<Square> squares = new ArrayList<Square>();
 
   // TODO: toString() - Return an ASCII representation of the aggregate board
   //  data
   @Override
   public String toString() {
-    return null;
+    return " a b c d e f g h i \n"
+        + " \u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550" +
+        "\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\n"
+        + "9\u2551 | | | | | | | | \u2551\n"
+        + " \u2551+-+-+-+-+-+-+-+-+\u2551\n"
+        + "8\u2551 | | | | | | | | \u2551\n"
+        + " \u2551+-+-+-+-+-+-+-+-+\u2551\n"
+        + "7\u2551 | | | | | | | | \u2551\n"
+        + " \u2551+-+-+-+-+-+-+-+-+\u2551\n"
+        + "6\u2551 | | | | | | | | \u2551\n"
+        + " \u2551+-+-+-+-+-+-+-+-+\u2551\n"
+        + "5\u2551 | | | | | | | | \u2551\n"
+        + " \u2551+-+-+-+-+-+-+-+-+\u2551\n"
+        + "4\u2551 | | | | | | | | \u2551\n"
+        + " \u2551+-+-+-+-+-+-+-+-+\u2551\n"
+        + "3\u2551 | | | | | | | | \u2551\n"
+        + " \u2551+-+-+-+-+-+-+-+-+\u2551\n"
+        + "2\u2551 | | | | | | | | \u2551\n"
+        + " \u2551+-+-+-+-+-+-+-+-+\u2551\n"
+        + "1\u2551 | | | | | | | | \u2551\n"
+        + " \u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550" +
+        "\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\n"
+        + "  a b c d e f g h i \n";
   }
 
 }
--- src/main/java/io/github/usafa_compsci350/QuoridorCli.java
+++ src/main/java/io/github/usafa_compsci350/QuoridorCli.java
@@ -1,103 +1,122 @@
 package io.github.usafa_compsci350;
 
+import java.util.Scanner;
+
 public class QuoridorCli implements Quoridor {
   // The Quoridor game's playing board
-  Board board;
+  private Board board;
 
   // Global exit flag
-  boolean exit = false;
+  private boolean exit = false;
+
+  // Global input Scanner
+  private Scanner sc;
+
+  // Global player count
+  public int numberOfPlayers;
 
   QuoridorCli() {
-    // TODO: Output Welcome message and handle any first-run initial setup then
-    //  - init()
+    System.out.println("Welcome to Quoridor!\n");
+    sc = new Scanner(System.in);
+    init();
   }
 
-  // TODO: init() - Initialize game
+  // init() - Initialize game.
   @Override
   public void init() {
-    // TODO: Initialize any default game values - board
-    // TODO: Get amount of players
-    // TODO: Start game
+    board = new Board();
+
+    System.out.println("How many players will there be (2 or 4)?");
+    // TODO: Handle errors/valid input test
+    numberOfPlayers = Integer.parseInt(getInput());
+
+    // TODO: Write out notation instructions/format for wall or pawn movement
+
+    // Draw the board
+    drawBoard();
   }
 
-  // TODO: play() - Game event loop handler
+  // play() - Game event loop handler
   @Override
   public void play() {
-    // TODO: Create game loop (while(!exit))
-    // TODO:    Conduct player turn, keeping track of player # and store exiting
-    //  condition - conductPlayerTurn()
+    while (!exit) {
+      for (int i = 1; i <= numberOfPlayers; i++) {
+        System.out.println("\nIt is Player " + i + "'s turn!");
+        exit = conductPlayerTurn(i);
+      }
+    }
+    // Exit the game application
+    exitGame();
   }
 
-  //   TODO: conductPlayerTurn() - Conducts a player's turn, returns an exit
-  //    application condition
-  //  private boolean conductPlayerTurn(int playerNumber) {
-  //    // TODO:    Get player Input - getInput()
-  //    // TODO:    Check the player move - checkMove() - Store an endState
-  //        boolean
-  //    // TODO:    Update/draw the game board - drawBoard()
-  //    // TODO:    Handle if endState condition - checkEndState()
-  //    // TODO:        Return boolean of exitGame()
-  //
-  //    // Return false by default to continue rotating player turns
-  //    return false;
-  //  }
-  //
-  //  // TODO: getInput() - Gets the player input from the command line,
-  //      returning a
-  //  //  string of their input
-  //  private String getInput() {
-  //    return null;
-  //  }
-  //
-  //  // TODO: checkMove() - Check for a valid wall placement/pawn movement
-  //      choice
-  //
-  //  //  returning a boolean for end state being met
-  //  private boolean checkMove(String input) {
-  //    // TODO: Prompt for player's move choice until a valid move is chosen
-  //     and
-  //    //  returned - while(true)
-  //     // TODO:    Obtain player input
-  //
-  //    // TODO:    Parse the user input for move choice type and validate the
-  //        move
-  //    //  (Convert form/notation if needed)
-  //    // TODO:        Check if the move choice is for placing a wall and
-  //        validate/
-  //    //  post data
-  //    // TODO:        Check if the move choice is for moving the pawn and
-  //        validate
-  //    //  /post data, return result boolean endState if reached
-  //
-  //    // Return false by default when game end state is not met from the
-  //    turn's
-  //    // move
-  //    return false;
-  //  }
-  //
-  //  // TODO: checkWallMove() - Check wall placement choice, post data if valid
-  //  private void checkWallMove(String input) {
-  //  }
-  //
-  //  // TODO: checkPawnMove() - Check pawn move choice, post data if valid,
-  //      return
-  //  //  boolean of end state being reached
-  //  private boolean checkPawnMove(String input) {
-  //    return false;
-  //  }
-  //
-  //  // TODO: checkEndState() - Return true/false for a game-ending condition
-  //      being
-  //  //  met
-  //  private boolean checkEndState() {
-  //    // TODO: Check for a game ending condition:
-  //    //  If found, prompt for replay choice and re-init game if chosen and
-  //     return
-  //    //  true for replay
-  //
-  //    // Return false by default if replay not chosen
-  //    return false;
-  //  }
+  // TODO: conductPlayerTurn() - Conducts a player's turn, returns an exit
+  //  application condition
+  private boolean conductPlayerTurn(int playerNumber) {
+    // Get and store user move choice input, using pre-defined notation*
+    System.out.println("Enter your move choice: ");
+    String input = getInput();
+
+    boolean endState = checkMove(input);
+    drawBoard();
+    if (endState) {
+      return checkEndState();
+    }
+
+    // Return false by default to continue rotating player turns
+    return false;
+  }
+
+
+  // getInput() - Gets the player input from the command line, returning a
+  //  string of their input
+  private String getInput() {
+    return sc.nextLine();
+  }
+
+  // TODO: checkMove() - Check for a valid wall placement/pawn movement choice,
+  //  returning a boolean for end state being met
+  private boolean checkMove(String input) {
+    // TODO: Prompt for player's move choice until a valid move is chosen and
+    //  returned - while(true)
+    // TODO:    Obtain player input
+
+    // TODO:    Parse the user input for move choice type and validate the move
+    //  (Convert form/notation if needed)
+    // TODO:        Check if the move choice is for placing a wall and validate/
+    //  post data
+    // TODO:        Check if the move choice is for moving the pawn and validate
+    //  /post data, return result boolean endState if reached
+
+    // Return false by default when game end state is not met from the turn's
+    // move
+    return false;
+  }
+
+  // TODO: checkWallMove() - Check wall placement choice, post data if valid
+  private void checkWallMove(String input) {
+  }
+
+  // TODO: checkPawnMove() - Check pawn move choice, post data if valid, return
+  //  boolean of end state being reached
+  private boolean checkPawnMove(String input) {
+    return false;
+  }
+
+  // TODO: checkEndState() - Return true/false for a game-ending condition being
+  //  met
+  private boolean checkEndState() {
+    // TODO: Check for a game ending condition:
+    //  If found, prompt for replay choice and re-init game if chosen and return
+    //  true for replay
+
+    // Return false by default if replay not chosen
+    return false;
+  }
+
+  // Draw the board to the console
+  private void drawBoard() {
+    System.out.println(board.toString());
+  }
 
   // TODO: exitGame() - Conduct any necessary game/App cleanup and exiting
   //  messages
